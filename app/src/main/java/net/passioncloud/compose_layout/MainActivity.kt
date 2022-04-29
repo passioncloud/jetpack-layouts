f93e249a9df94cdaeefc12a8bb34c5e76adebb9b
Gilbert <gssenyonjo@gmail.com>
2022-04-29 06:36:52 +0300

    Looked at Scaffold composable

--- app/src/main/java/net/passioncloud/compose_layout/MainActivity.kt
+++ app/src/main/java/net/passioncloud/compose_layout/MainActivity.kt
@@ -1,7 +1,6 @@
 package net.passioncloud.compose_layout
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,12 +9,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,10 +27,88 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotographerCardPreview()
+            MyAppPreview()
         }
     }
 }
+
+@Composable
+fun MyApp() {
+    Scaffold(
+        topBar = { MyAppBar() },
+        drawerContent = { MyDrawerContent() },
+        bottomBar = { MyBottomAppBar() }
+    ) { innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun MyBottomAppBar() {
+    BottomAppBar {
+        BottomNavigationItem(
+            selected = true,
+            onClick = { },
+            icon = {
+                Icon(Icons.Filled.Add, contentDescription = "Add one")
+            })
+        BottomNavigationItem(
+            selected = true,
+            onClick = { },
+            icon = {
+                Icon(Icons.Filled.Star, contentDescription = "Star me")
+            }
+        )
+    }
+}
+
+@Composable
+fun MyDrawerContent() {
+    ModalDrawer(drawerContent = {
+        Text("Minute main")
+        Text("Coca cola")
+    }) {
+
+    }
+}
+
+@Composable
+fun MyAppBar() {
+    TopAppBar(
+        title = {
+            Text(text = "Welcome to our app")
+        },
+        actions = {
+            IconButton(onClick = {/* Do sth */ }) {
+                Icon(Icons.Filled.Favorite, contentDescription = "My fav")
+            }
+        }
+    )
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        Text(
+            text = "Gwe musajja"
+        )
+        Text("When did you start college")
+    }
+}
+
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposelayoutTheme {
+        MyApp();
+    }
+}
+
 
 @Composable
 fun PhotographerCard(modifier: Modifier = Modifier) {
@@ -85,21 +165,12 @@ fun MyBtn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
     }
 }
 
-@Composable
-fun MyTopAppBar() {
-    TopAppBar(
-        title = {
-            Text(text = "My nice app bar", maxLines = 2)
-        }
-    )
-}
 
 @Preview
 @Composable
 fun PhotographerCardPreview() {
     ComposelayoutTheme {
         PhotographerCard()
-        MyTopAppBar()
     }
 }
 
